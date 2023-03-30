961ef866c2e04493fb5142b4ba1dafef426b0fdf
Amit Twito <amit6991@gmail.com>
2023-03-30 15:30:11 +0300

    updated requirements.txt

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ max_tests_number = 10
 configuration_creator_app = ConfigurationCreatorApp()
 configuration_creator_app.run(config_file_path=config_file_path, max_tests_number=max_tests_number,
                               number_of_sections_to_randomize=number_of_sections_to_randomize, port=port, width=1200,
-                              height=800, )
+                              height=800,)
